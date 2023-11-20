40deb63466717dd4ada994c460109b2e0410ab21
Malack15 <malackokeiga@gmail.com>
2023-11-20 10:30:56 -0800

    a function that prints an integer

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-
 def safe_print_integer(value):
-try:
+    try:
         print("{:d}".format(value))
-        return (True)
-    except (TypeError, ValueError):
-        return (False)
+    except(ValueError, TypeError):
+        return False
+    else:
+        return True
