da268aa0367362128c0b2dc96d9c6861663f7997
Malack15 <malackokeiga@gmail.com>
2023-11-28 15:50:31 +0300

    update changes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """
+a function that divides all elements of a matrix
 defines function to scalar divde matrix
 """
 
@@ -7,18 +8,17 @@ defines function to scalar divde matrix
 def matrix_divided(matrix, div):
     """divides matrix by scalar integer, rounded to two decimal places"""
     import decimal
-    error_msg = "matrix must be a matrix (list of lists) of integers/floats"
     if type(matrix) is not list:
-        raise TypeError(error_msg)
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
     len_rows = []
     row_count = 0
     for row in matrix:
         if type(row) is not list:
-            raise TypeError(error_msg)
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         len_rows.append(len(row))
         for element in row:
             if type(element) not in [int, float]:
-                raise TypeError(error_msg)
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
         row_count += 1
     if len(set(len_rows)) > 1:
         raise TypeError("Each row of the matrix must have the same size")
